4c1c47b9fb34c43903cdb30003670cc6161e9c29
sun-yryr <taittide@gmail.com>
2020-04-27 16:50:51 +0900

    pathの調整

--- bin/symboliclink.sh
+++ bin/symboliclink.sh
@@ -4,6 +4,6 @@
 DOTFILES_PATH=$(cd $(dirname $0); pwd)
 
 # シンボリックリンクを貼る
-ln -sf "$DIR_PATH/.xonshrc" $HOME
+ln -sf "$DOTFILES_PATH/../.xonshrc" $HOME
 mkdir -p "$HOME/.ssh"
-cp "$DIR_PATH/ssh_config" "$HOME/.ssh/config"
+cp "$DOTFILES_PATH/../ssh_config" "$HOME/.ssh/config"
