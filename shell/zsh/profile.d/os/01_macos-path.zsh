2c0d81669635bcaa0df62195e68d2bdcdb94291d
sun-yryr <me@sun-yryr.com>
2026-02-05 12:23:02 +0900

    片栗粉

--- shell/zsh/profile.d/os/01_macos-path.zsh
+++ shell/zsh/profile.d/os/01_macos-path.zsh
@@ -11,6 +11,17 @@ if [ -f "~/.swiftly/env.sh" ]; then
     . ~/.swiftly/env.sh
 fi
 
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init.zsh 2>/dev/null || :
+# あれば
+if [ -f "~/.orbstack/shell/init.zsh" ]; then
+  # Added by OrbStack: command-line tools and integration
+  # This won't be added again if you remove it.
+  source ~/.orbstack/shell/init.zsh 2>/dev/null || :
+fi
+
+# あれば
+if [ -f "~/.lmstudio/bin" ]; then
+  # Added by LM Studio CLI (lms)
+  export PATH="$PATH:/Users/taiyo_minagawa_yume/.lmstudio/bin"
+  # End of LM Studio CLI section
+fi
+
